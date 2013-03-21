5acf71e535dd38c3c487e72d3c868d567f0532da
Zhongxin Huang <huangzhongxin@ivali.com>
2013-03-21 09:29:45 +0800

    archlinux_search.py 支持多页

--- network/archlinux_search.py
+++ network/archlinux_search.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-# -*- utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 
 import sys
+import argparse
 from bs4 import BeautifulSoup
 from getUrlResponse import getUrlResponse
-import argparse
 
-__version__ = '0.1'
+VERSION = '0.1'
+
 
 def do_search(pkg):
     ''' search archlinux packages
@@ -16,23 +17,35 @@ def do_search(pkg):
       pkg: A string, pkgname
 
     '''
+    # 获得页数, 默认为 1
+    pageNum = 1
     url = 'https://www.archlinux.org/packages/?q={}'.format(pkg)
     respon = getUrlResponse(url)
     soup = BeautifulSoup(respon)
-    if _debug_:
+    if DEBUG:
         sys.stderr.write(soup.prettify(encoding='utf-8'))
-    print(soup.find(attrs={'class': 'pkglist-stats'}).p.text) + '\n'
-    find_pkg = []
-    for x in soup.find_all('td'):
-        find_pkg.append(x.text)    
-        #if x.a:
-        #    find_pkg.append(x.a['href'])
+    pkglist = soup.find(attrs={'class': 'pkglist-stats'}).p.text
+    print(pkglist + '\n')
+    if 'Page' in pkglist:
+        # 原始数据是：'144 packages found. Page 1 of 2.'
+        pageNum = int(pkglist.split()[-1].rstrip('.'))
+    # 逐页输出
     print('Arch    Repo    Name    Version    Description    Last Updated\n')
-    for x in find_pkg:
-        if x == '':
-            print('\n')
-            continue
-        print('{:<6}'.format(x)),
+    for num in range(1, pageNum + 1):
+        if num >= 2:
+            url = 'https://www.archlinux.org/packages/?page={}&q={}'.format(num, pkg)
+            respon = getUrlResponse(url)
+            soup = BeautifulSoup(respon)
+        find_pkg = []
+        for x in soup.find_all('td'):
+            find_pkg.append(x.text)    
+            #if x.a:
+            #    find_pkg.append(x.a['href'])
+        for x in find_pkg:
+            if x == '':
+                print('\n')
+                continue
+            print('{:<6}'.format(x)),
 
 
 if __name__ == '__main__':
@@ -49,10 +62,10 @@ if __name__ == '__main__':
                         dest='s', help='search pkgname')
     args = parser.parse_args(argvs)
     if args.v:
-        print(__version__)
-    _debug_ = False
+        print(VERSION)
+    DEBUG = False
     if args.d:
-        _debug_ = True
+        DEBUG = True
     if args.s:
         for pkg in args.s:
             do_search(pkg)
