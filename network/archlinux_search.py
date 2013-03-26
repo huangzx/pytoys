38dace06e5df42a3cde7c7b8ccc3c4cc1a148197
Zhongxin Huang <huangzhongxin@ivali.com>
2013-03-26 23:03:24 +0800

    archlinux_search.py: 调整输出，代码规范(pep8)

--- network/archlinux_search.py
+++ network/archlinux_search.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# Author: Zhongxin Huang <zhongxin.huang@gmail.com>
+#
 
 import sys
 import os
@@ -34,7 +36,7 @@ def do_search(givenName):
     #
     # 输出软件包信息
     #
-    print('Arch    Repo    Name    Version    Description    Last Updated\n')
+    print('Arch    Repo    Name    Version    Description    Last Updated   Flag Date\n')
     for num in range(1, pageNum + 1):
         if num > 1:
             url = 'https://www.archlinux.org/packages/?page={}&q={}'.format(num, givenName)
@@ -47,30 +49,15 @@ def do_search(givenName):
                 ret = x.a['href']
                 if not ret in find_suburl:
                     find_suburl.append(ret)
-        # 输出 7 行回车一次
-        for num, value in enumerate(find_pkg):
-            print num, value
-            if num % 6:
-
-            if x == '':
-                print('\n')
-                continue
-            print('{:<6}'.format(x)),
+        # 每 7 行换行
+        for n in xrange(7, len(find_pkg)+7, 7):
+            print ' '.join(find_pkg[n-7:n]),
+            print '\n'
     #
     # 获得 PKGBUILD 等文件
     #
     if not FETCH:
         return
-    # 如果指定了下载目录
-    if SAVEDIR:
-        path = os.path.realpath(SAVEDIR)
-        if not os.path.exists(path):
-            os.makedirs(path)
-        os.chdir(path)
-    else:
-        #path = os.getcwd()
-        path = '/tmp'
-    print 'Download to {}'.format(path)
     find_sourceurl = []
     for suburl in find_suburl:
         #arch = suburl.split('/')[3]
@@ -84,8 +71,17 @@ def do_search(givenName):
             # 如果要求严格匹配软件包名
             if STRICT and (basepkg != givenName):
                 continue
+            # 如果指定了下载目录
+            pathSave = '/tmp'
+            if SAVEDIR:
+                pathSave = os.path.realpath(SAVEDIR)
+            pathSave = os.path.join(pathSave, basepkg)
+            if not os.path.exists(pathSave):
+                os.makedirs(pathSave)
+            os.chdir(pathSave)
             plainUrl = sourceUrl.split('/tree/')[0] + '/plain/' + basepkg + '/trunk/'
             print "Fetching from {}".format(plainUrl)
+            print 'Download to {}'.format(pathSave)
             soup = cookSoup(plainUrl)
             for x in soup.find_all('li'):
                 fileUrl = x.a['href']
@@ -95,6 +91,7 @@ def do_search(givenName):
                     fileSave = fileUrl.split('/')[-1]
                     print 'Get: {}'.format(fileSave)
                     downloadFile(fileUrl, fileSave, needReport=False)
+            print('\n')
             if STRICT and (basepkg == givenName):
                     break
 
@@ -103,16 +100,17 @@ if __name__ == '__main__':
     argvs = sys.argv[1:]
     if not argvs:
         argvs = ['-h']
-    desc = 'Search package in Arch Linux Packages, a.k.a https://www.archlinux.org/packages.'
+    desc = ('Search package in Arch Linux Packages, '
+            'a.k.a https://www.archlinux.org/packages.')
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('-v', '--version', action='store_true',
-                        dest='v', help='show the version number and exit')
-    parser.add_argument('-f', '--fetch', action='store_true',
-                        dest='f', help='download PKGBUILD and other related files')
-    parser.add_argument('-s', '--strict', action='store_true',
-                        dest='s', help='download strictly match the package name')
-    parser.add_argument('-d', '--dir', nargs=1, metavar='dir',
-                        dest='d', help='download directory')
+    parser.add_argument('-v', '--version', action='store_true', dest='v', 
+                        help='show the version number and exit')
+    parser.add_argument('-f', '--fetch', action='store_true', dest='f', 
+                        help='download PKGBUILD and other related files')
+    parser.add_argument('-s', '--strict', action='store_true', dest='s', 
+                        help='download strictly match the package name')
+    parser.add_argument('-d', '--dir', nargs=1, metavar='dir', dest='d', 
+                        help="download directory, defaults to '/tmp'")
     parser.add_argument('pkg', nargs='*', help='search query')
     args = parser.parse_args(argvs)
     if args.v:
