824675ec94221f7c1b7995ad3b17e2cf6a2695b9
Zhongxin Huang <huangzhongxin@ivali.com>
2013-03-26 18:08:48 +0800

    修改默认下载地址为'/tmp'

--- network/archlinux_search.py
+++ network/archlinux_search.py
@@ -24,7 +24,7 @@ def do_search(givenName):
     soup = cookSoup(url)
     try:
         pkglist = soup.find(attrs={'class': 'pkglist-stats'}).p.text
-    except AttributeError:   
+    except AttributeError:
         sys.stderr.write("'{}' not found at Arch Packages.\n".format(givenName))
         return 1
     print(pkglist + '\n')
@@ -47,7 +47,11 @@ def do_search(givenName):
                 ret = x.a['href']
                 if not ret in find_suburl:
                     find_suburl.append(ret)
-        for x in find_pkg:
+        # 输出 7 行回车一次
+        for num, value in enumerate(find_pkg):
+            print num, value
+            if num % 6:
+
             if x == '':
                 print('\n')
                 continue
@@ -55,58 +59,61 @@ def do_search(givenName):
     #
     # 获得 PKGBUILD 等文件
     #
-    if FETCH:
-        # 如果指定了下载目录
-        if SAVEDIR:
-            path = os.path.realpath(SAVEDIR)
-            if not os.path.exists(path):
-                os.makedirs(path)
-            os.chdir(path)
-        else:
-            path = os.getcwd()
-        print 'Download to {}'.format(path)
-        find_sourceurl = []
-        for suburl in find_suburl:
-            arch = suburl.split('/')[3]
-            name = suburl.split('/')[4]
-            pkgUrl = 'https://www.archlinux.org' + suburl
-            soup = cookSoup(pkgUrl)
-            sourceUrl = soup.find(attrs={'title': 'View source files for {}'.format(name)})['href']
-            if not sourceUrl in find_sourceurl:
-                find_sourceurl.append(sourceUrl)
-                basepkg = sourceUrl.split('/')[-1]
-                # 如果要求严格匹配软件包名
-                if STRICT:
-                    if basepkg != givenName:
-                        continue
-                plainUrl = sourceUrl.split('/tree/')[0] + '/plain/' + basepkg + '/trunk/'
-                print "Fetching from {}".format(plainUrl)
-                soup = cookSoup(plainUrl)
-                for x in soup.find_all('li'):
-                    fileUrl = x.a['href']
-                    if not fileUrl.endswith('/'):
-                        # 每一个文件的真实 URL
-                        fileUrl = 'https://projects.archlinux.org' + fileUrl
-                        fileSave = fileUrl.split('/')[-1]
-                        print 'Get: {}'.format(fileSave)   
-                        downloadFile(fileUrl, fileSave, needReport=False)
+    if not FETCH:
+        return
+    # 如果指定了下载目录
+    if SAVEDIR:
+        path = os.path.realpath(SAVEDIR)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        os.chdir(path)
+    else:
+        #path = os.getcwd()
+        path = '/tmp'
+    print 'Download to {}'.format(path)
+    find_sourceurl = []
+    for suburl in find_suburl:
+        #arch = suburl.split('/')[3]
+        name = suburl.split('/')[4]
+        pkgUrl = 'https://www.archlinux.org' + suburl
+        soup = cookSoup(pkgUrl)
+        sourceUrl = soup.find(attrs={'title': 'View source files for {}'.format(name)})['href']
+        if not sourceUrl in find_sourceurl:
+            find_sourceurl.append(sourceUrl)
+            basepkg = sourceUrl.split('/')[-1]
+            # 如果要求严格匹配软件包名
+            if STRICT and (basepkg != givenName):
+                continue
+            plainUrl = sourceUrl.split('/tree/')[0] + '/plain/' + basepkg + '/trunk/'
+            print "Fetching from {}".format(plainUrl)
+            soup = cookSoup(plainUrl)
+            for x in soup.find_all('li'):
+                fileUrl = x.a['href']
+                if not fileUrl.endswith('/'):
+                    # 每一个文件的真实 URL
+                    fileUrl = 'https://projects.archlinux.org' + fileUrl
+                    fileSave = fileUrl.split('/')[-1]
+                    print 'Get: {}'.format(fileSave)
+                    downloadFile(fileUrl, fileSave, needReport=False)
+            if STRICT and (basepkg == givenName):
+                    break
 
 
 if __name__ == '__main__':
     argvs = sys.argv[1:]
     if not argvs:
         argvs = ['-h']
-    desc = 'Search package in https://www.archlinux.org/packages.'
+    desc = 'Search package in Arch Linux Packages, a.k.a https://www.archlinux.org/packages.'
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-v', '--version', action='store_true',
-                        dest='v', help='displays the version number')
+                        dest='v', help='show the version number and exit')
     parser.add_argument('-f', '--fetch', action='store_true',
-                        dest='f', help='download PKGBUILD and other related documents')
+                        dest='f', help='download PKGBUILD and other related files')
     parser.add_argument('-s', '--strict', action='store_true',
                         dest='s', help='download strictly match the package name')
     parser.add_argument('-d', '--dir', nargs=1, metavar='dir',
                         dest='d', help='download directory')
-    parser.add_argument('pkg', nargs='*', help='search the package name')
+    parser.add_argument('pkg', nargs='*', help='search query')
     args = parser.parse_args(argvs)
     if args.v:
         print(VERSION)
